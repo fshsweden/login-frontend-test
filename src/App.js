6eb9be00b23dda8507acb55f1dc0088fead2b595
Peter Andersson <fshsweden@gmail.com>
2022-06-08 23:39:28 +0200

    Finished demo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import { Form, Button } from "react-bootstrap";
+import { useState } from 'react';
+
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const [errorMessages, setErrorMessages] = useState({});
+    // const [isSubmitted, setIsSubmitted] = useState(false);
+    // const loggedIn = localStorage.getItem("access_token") == null;
+    const [token, setToken] = useState(0)
+    const [text, setText] = useState({ text: "", color: "black" })
+    const [loggedIn, setLoggedIn] = useState(false) // { token } != null ? true : false
+
+    console.log("access_token: ", token)
+    console.log("logged in: ", loggedIn)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(event);
+
+        // Prevent page reload
+        const email_value = event.target.name.value
+        const password_value = event.target.password.value
+
+        const info = {
+            email: email_value,
+            password: password_value
+        }
+
+        try {
+            await fetch("http://192.168.1.67:5000/login",
+                {
+                    method: "POST",
+                    body: JSON.stringify(info),
+                    headers: {
+                        "Content-Type": "application/json;charset=UTF-8",
+                        "Accept": "application/json"
+                    }
+                })
+                .then((response) => {
+                    const statusCode = response.status;
+                    const data = response.json();
+                    return Promise.all([statusCode, data]);
+                })
+                .then((data) => {
+                    data = data[1];
+                    if (data.access_token) {
+                        setToken(data.access_token); // Temp!
+                        localStorage.setItem("access_token", data.access_token);
+                        setText({
+                            text: "Sucessfully logged in!",
+                            color: "green"
+                        });
+                        setLoggedIn(true);
+                    } else {
+                        setText({
+                            text: "Incorrect email or password",
+                            color: "red"
+                        });
+                    }
+
+                });
+        } catch (err) {
+            console.error(err);
+        }
+
+
+    };
+
+    const handleProfileClick = (event) => {
+
+        event.preventDefault();
+
+        try {
+            fetch("http://192.168.1.67:5000/profile",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json;charset=UTF-8",
+                        "Accept": "application/json",
+                        "Authorization": "Bearer " + token
+                    }
+                })
+                .then((response) => {
+                    const statusCode = response.status;
+                    console.log(statusCode)
+                    const data = response.json();
+                    console.log(data)
+                    return Promise.all([statusCode, data]);
+                })
+                .then((data) => {
+                    console.log(data);
+                    data = data[1]
+                    if (data.about) {
+                        setText({ text: data.about, color: "black" })
+                    }
+
+                });
+        } catch (err) {
+            console.error(err);
+        }
+
+
+    };
+
+    const handleLogout = (event) => {
+        event.preventDefault();
+
+        try {
+            fetch("http://192.168.1.67:5000/logout",
+                {
+                    method: "POST",
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                })
+                .then((response) => {
+                    const statusCode = response.status;
+                    const data = response.json();
+                    return Promise.all([statusCode, data]);
+                })
+                .then((data) => {
+                    console.log(data)
+                    localStorage.removeItem("access_token");
+                    setToken(0);
+                    setText({
+                        text: "Succesfully logged out!",
+                        color: "green"
+                    })
+                    setLoggedIn(false)
+                })
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const RenderLoggedInState = () => {
+        return <div>
+                <h1>You are logged in!</h1>
+                <button onClick={handleProfileClick}>Profile</button>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+    }
+
+    const renderForm = () => {
+        return <div className="login-form">
+            <div className="form">
+                <div className="title">Sign In</div>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-container">
+                        <label>Username </label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div className="input-container">
+                        <label>Password</label>
+                        <input type="password" name="password" />
+                    </div>
+                    <div className="button-container">
+                        <input type="submit" value="Login" />
+                    </div>
+                </form>
+            </div>
+            <div>
+                <h5>Do you have an invitation code? If so, signup here!</h5>
+            </div>
+        </div>
+    }
+
+    return (<div className="app">
+        {token === 0 ? renderForm() : RenderLoggedInState()}
+        {text.text}
+    </div>);
 }
 
 export default App;
+
+
